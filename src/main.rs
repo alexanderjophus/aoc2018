ea2f23069b7bf104932d449c0b6570bfa6abfd87
Alexander J Trelore <alextrelore@gmail.com>
2018-12-02 00:31:56 +0000

    day 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::collections::HashSet;
+
 fn main() {
-    println!("Hello, world!");
+    day1()
+}
+
+fn day1() {
+    let input = include_str!("day1.txt");
+        
+    let p1: i32 = day1p1(input);
+    let p2: i32 = day1p2(input);
+
+    println!("p1: {}\np2: {}", p1, p2);
+}
+
+fn day1p1(input: &str) -> i32 {
+    input.lines()
+        .map(|x| x.parse::<i32>().expect("can't parse value"))
+        .sum()
+}
+
+fn day1p2(input: &str) -> i32 {
+    let mut seen = HashSet::new();
+	let mut sum = 0 as i32;
+	input.lines()
+        .cycle()
+        .find_map(|x| {
+            sum += x.parse::<i32>().expect("can't parse value");
+            seen.replace(sum)
+        }).unwrap()
 }
